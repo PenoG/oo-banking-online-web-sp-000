99788ff0ca53c569126ce1b9c434d36de7e4a6bd
Mason Phillips <mjphillips2000@gmail.com>
2020-11-03 21:13:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,4 +12,12 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end
+  
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
 end
